6e7c76e18be0d6ae6bf43b34403d0081a0b57329
Racyn Ibaya <racynibaya@outlook.com>
2023-05-08 14:23:22 -0400

    advance filtering, loader, error page, middleware for protecting routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import ClientOnly from './components/ClientOnly';
 import './globals.css';
 import ToasterProvider from './providers/ToasterProvider';
 import getCurrentUser from './actions/getCurrentUser';
+import SearchModal from './components/modals/SearchModal';
 
 export const metadata = {
   title: 'AirBnb',
@@ -31,6 +32,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <SearchModal />
           <RentModal />
           <LoginModal />
           <RegisterModal />
